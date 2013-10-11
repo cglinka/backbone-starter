dacd0001505e5019084a60a5f525260cd21d6e25
Clare Glinka <glinka.cb@gmail.com>
2013-10-11 11:21:03 -0500

    Remove old code, add instances of Inventory collection, StoreView view, add items to inventory.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,29 @@
 (function () {
   /* TODO: WRITE CODE HERE */
 
-  var product = new Product({
-    name: 'A PRODUCT NAME',
-    price: 10.99
+  // var product = new Product({
+  //   name: 'A PRODUCT NAME',
+  //   price: 10.99
+  // });
+
+  // var productView = new ProductView({
+  //   model: product
+  // });
+
+  // productView.render();
+
+  // console.log('productView.el', productView.el);
+  // $("#store").append(productView.el);
+
+  var inventory = new Inventory({});
+  var storeView = new StoreView({
+    collection: inventory,
+    el: '#store'
   });
 
-  var productView = new ProductView({
-    model: product
-  });
-
-  productView.render();
-
-  console.log('productView.el', productView.el);
-  $("#store").append(productView.el);
+  inventory.add([
+    {name: 'Fancy Hat', price: 14.99},
+    {name: 'Boring Hat', price: 10.80},
+    {name: 'Fun Hat', price: 5.50}
+    ]);
 })();
