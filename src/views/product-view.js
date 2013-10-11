9e0a1909b3c3ae65adece18b926bd04df8c63cc4
Clare Glinka <glinka.cb@gmail.com>
2013-10-11 11:18:41 -0500

    Add comments, className to product-view.js.

--- src/views/product-view.js
+++ src/views/product-view.js
@@ -1,11 +1,12 @@
 (function () {
 
   window.ProductView = Backbone.View.extend({
-    // initialize: function (options) {
-    //   this.bar = 'init val';
-    // }
+    className: 'product',
+
+    // Fetch the product template from index.html
     template: _.getTemplate('product'),
 
+    // Add model information to the view's div.
     render: function () {
       var newProductHtml = this.template( this.model.toJSON() );
       $(this.el).html(newProductHtml);
